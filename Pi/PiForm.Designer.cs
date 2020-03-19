814d05670a9f56248c23cd46bdd6c6beb5ad662d
Joshua Oh <cfvote@outlook.com>
2020-03-18 20:47:25 -0700

    Add project files.

---
+++ Pi/PiForm.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Pi
+{
+    partial class PiForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this._digits = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this._backgroundButton = new System.Windows.Forms.Button();
+            this._pi = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnAPM = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this._performance = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this._digits)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _digits
+            // 
+            this._digits.Location = new System.Drawing.Point(242, 67);
+            this._digits.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this._digits.Name = "_digits";
+            this._digits.Size = new System.Drawing.Size(472, 31);
+            this._digits.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 79);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Digits of Pi:";
+            // 
+            // _backgroundButton
+            // 
+            this._backgroundButton.Location = new System.Drawing.Point(68, 140);
+            this._backgroundButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this._backgroundButton.Name = "_backgroundButton";
+            this._backgroundButton.Size = new System.Drawing.Size(242, 44);
+            this._backgroundButton.TabIndex = 2;
+            this._backgroundButton.Text = "Background Worker";
+            this._backgroundButton.UseVisualStyleBackColor = true;
+            this._backgroundButton.Click += new System.EventHandler(this._backgroundButton_Click);
+            // 
+            // _pi
+            // 
+            this._pi.Location = new System.Drawing.Point(68, 244);
+            this._pi.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this._pi.Multiline = true;
+            this._pi.Name = "_pi";
+            this._pi.Size = new System.Drawing.Size(954, 154);
+            this._pi.TabIndex = 3;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnAPM
+            // 
+            this.btnAPM.Location = new System.Drawing.Point(322, 140);
+            this.btnAPM.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnAPM.Name = "btnAPM";
+            this.btnAPM.Size = new System.Drawing.Size(212, 44);
+            this.btnAPM.TabIndex = 4;
+            this.btnAPM.Text = "APM";
+            this.btnAPM.UseVisualStyleBackColor = true;
+            this.btnAPM.Click += new System.EventHandler(this.btnAPM_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(546, 140);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 44);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Task";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(800, 140);
+            this.button3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(218, 44);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Parallel Task";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 198);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Results:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 467);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Performance:";
+            // 
+            // _performance
+            // 
+            this._performance.Location = new System.Drawing.Point(222, 467);
+            this._performance.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this._performance.Multiline = true;
+            this._performance.Name = "_performance";
+            this._performance.Size = new System.Drawing.Size(800, 35);
+            this._performance.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(68, 556);
+            this.button4.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 44);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // PiForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1138, 1088);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this._performance);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnAPM);
+            this.Controls.Add(this._pi);
+            this.Controls.Add(this._backgroundButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._digits);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "PiForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this._digits)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown _digits;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button _backgroundButton;
+        private System.Windows.Forms.TextBox _pi;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnAPM;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox _performance;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Pi/PiForm.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Pi
+{
+    public partial class PiForm : Form
+    {
+        PiObject pi;
+
+        public PiForm()
+        {
+            InitializeComponent();
+            this.Text = "Digits of Pi";
+            _digits.Maximum = decimal.MaxValue;
+            _pi.ScrollBars = ScrollBars.Both;
+            _pi.WordWrap = true;
+            _digits.Text = "1";
+        }
+
+        private void _backgroundButton_Click(object sender, EventArgs e)
+        {            
+            if (this._backgroundButton.Text == "Cancel")
+            {
+                timer1.Stop();
+                _backgroundButton.Text = "Calc";
+                _digits.Enabled = true;
+                backgroundWorker1.CancelAsync();
+                return;
+            }
+            else
+
+            {
+                timer1.Start();
+                
+
+                pi = new PiObject
+                {
+                    calcValue = "",
+                    digits = (int)_digits.Value,
+                    
+                };
+
+                _digits.Enabled = false;
+                
+                _backgroundButton.Text = "Cancel";
+                _pi.Text = string.Empty;
+                
+
+                backgroundWorker1.RunWorkerAsync(pi);
+
+            }
+        }
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            PiObject piObj = e.Argument as PiObject;
+            backgroundWorker1 = sender as BackgroundWorker;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            backgroundWorker1.WorkerReportsProgress = true;
+
+            StringBuilder pi = new StringBuilder("3", piObj.digits + 2);
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            if (piObj.digits > 0)
+            {
+                pi.Append(".");
+
+                for (int i = 0; i < piObj.digits; i += 9)
+                {
+                    if (!this.backgroundWorker1.CancellationPending)
+                    {
+                        int nineDigits = NineDigitsofPi.StartingAt(i + 1);
+                        int digitCount = Math.Min(piObj.digits - i, 9);
+                        String ds = String.Format("{0:D9}", nineDigits);
+                        pi.Append(ds.Substring(0, digitCount));
+                        piObj.calcValue = pi.ToString();
+                        
+                    }
+                    else
+                    {
+                        e.Cancel = true;
+                    }
+
+                }// the code that you want to measure comes here
+                watch.Stop();
+            }
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            piObj.elapsedMs = elapsedMs.ToString() + " ms";
+
+
+        }
+
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            
+            _pi.Text = (string)e.UserState;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this._backgroundButton.Text = "Calc";
+            _digits.Enabled = true;
+            
+            if (e.Cancelled)
+            {
+                this._pi.Text = pi.calcValue + " <<cancelled>>";
+            }
+
+            
+            
+            
+
+
+        }
+
+        class PiObject
+        {
+            public String calcValue { get; set; }
+            public int digits { get; set; }
+            public String elapsedMs { get; set; }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _pi.Text = pi.calcValue;
+            _performance.Text = pi.elapsedMs;
+
+
+        }
+
+        private void btnAPM_Click(object sender, EventArgs e)
+        {
+            _pi.Text = "pi results";
+            _performance.Text = "0.1";
+        }
+    }
+
+}
